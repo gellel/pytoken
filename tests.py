0d7e921522557e383db78fae9f2fddb807f10f4a
gellel <lindsay.gelle@gmail.com>
2016-06-28 17:39:16 +1000

    Updated tests.py with more helper classes and functions

--- tests.py
+++ tests.py
@@ -1,16 +1,29 @@
 ### python scripts dependencies
 ### py selenium browser package
-##from selenium import webdriver
+### from selenium import webdriver
+
+### py subprocess class package
+import subprocess
 ### py textwrap class package
 import textwrap
+### py temp class package
+import tempfile
 ### py user class package
 import getpass
+### py urllib2 class package
+import urllib2
+### py import base64 package
+import base64
 ### py system class package
 import sys
 ### py os class package
 import os
 ### py regex
 import re
+
+
+### set localfile path
+__filepath__ = os.path.dirname(os.path.realpath('__file__'))
 
 ### prints prettier strs
 class String:
@@ -156,7 +169,8 @@ class Request:
 			return False
 		### prompt user that the supplied input wasn't considered valid
 		else:
-			print self.system.response(String().concat("command", String({'str': String(self.response).tag(), 'attr':{'weight':'bold'}}).get(), "unrecognised"))
+			print self.__resp__(String().concat("command", String({'str':String(str(self.response)).tag(),'attr':{'weight':'bold'}}).get(), "unrecognised"))
+	
 			### recall the function
 			return self.open()
 	### format the strings defining the options available for the user
@@ -212,47 +226,194 @@ class Package:
 		self.system = Responder()
 
 
-
+### general class designed to install python modules
 class Install:
-
+	### attempt to fetch all packages
 	def get (self):
+		### assign empty or reduced list
 		packages = self.__asgn__()
-
+		### if list is empty assume all packages were found
 		if not len(packages):
+			### return True for handler
 			return True
+		### attempt to install the missing files
 		else:
-			print self.system.response(String().concat(String({'str': String(str(len(packages)) + "/" + str(len(self.packages))).tag(), 'attr':{'color':'red','weight':'bold'}}).get(), "packages are missing"))
-
+			### ask user if they would like to install the files dependencies
+			print self.__resp__(String().concat(String({'str': String(str(len(packages)) + "/" + str(len(self.packages))).tag(), 'attr':{'color':'red','weight':'bold'}}).get(), "packages are missing"))
+			### if user agrees attempt to install each package
 			if Request(prompt = "attempt to install missing files?").open():
-				pass
-			else:
-				pass
+				for i in range(0, len(packages)):
+					self.__istl__(packages[i])
+			###
+			return False
 
-
+	### attempt to install the package through the supplied installer
 	def __istl__ (self, package):
-		print package
+		if hasattr(package['installer'], '__call__'):
+				file = package['installer']()
 
+		#print self.__resp__(String().concat("installer for", String(package['package'].name).tag(), "not supplied"))
+
+	### attempt to load the class package through Package.get() 		
 	def __atmp__ (self, package):
+		### return result
 		return package.get()
-
+	### reduce array if missing packages are found
 	def __asgn__ (self):
+		### iterate through self.supplied list
 		for i in range(0, len(self.packages)):
+			### attempt to import package through class method of get from "Package"
 			self.packages[i]['installed'] = True if self.__atmp__(self.packages[i]['package']) else False
-
+		### return reduced array
 		return [pckg for pckg in self.packages if not self.packages[i]['installed']]
 		
-
+	### substitute array item to be a dict with a class instance
 	def __pckg__ (self, packages):
 		for i in range(0, len(packages)):
-			packages[i] = {'package': Package(name = packages[i]['name'], source = packages[i]['source'])}
-		return packages
+			packages[i]['package'] = Package(name = packages[i]['name'], source = packages[i]['source'])
 
+		return packages
+	### format a Responder response
+	def __resp__ (self, string):
+		return self.system.response(string)
+	### constructor
 	def __init__ (self, packages = []):
 		self.packages = self.__pckg__(packages)
 		self.system = Responder()
 
 
+### creates either a temporary or permanently write file
+class File:
+	def encode (self):
+		return base64.b64encode(b + self.read())
+	def decode (self):
+		return base64.b64decode(b + self.read())
+	### removes file instance from os directory
+	def remove (self):
+		os.remove(self.file.name)
+	### returns the file entity
+	def get (self):
+		return self.file
+	### returns the contents of a file if file
+	def read (self, seeker = 0):
+		self.file.seek(seeker)
+		return self.file.read()
+	### writes to file, optional seeker position
+	def write (self, contents = 'print "Hello"', seeker = 0):
+		if re.compile('[rwa]\+?', re.IGNORECASE).match(self.file.mode):
+			self.file.seek(seeker)
+			self.file.write(contents)
+		else:
+			print "file mode doesn't allow writing"
+	def seek (self):
+		self.file.seek(0)
+	### creates file as a temporary file
+	def __mktf__ (self):
+		return tempfile.NamedTemporaryFile(suffix = self.ext, mode = self.mode, dir = self.filepath, delete = c)
+	### creates file as a saved file
+	def __mkfl__ (self):
+		### update to allow file creation
+		self.mode = "w+"
+		return open(os.path.join(self.filepath, (self.name + self.ext)), self.mode)
+	### select between temp or permanent file
+	def __crte__ (self):
+		file = None
+		if self.temporary:
+			file = self.__mktf__()
+		else:
+			file = self.__mkfl__()
+		os.chmod(file.name, 0777)
+		return file
+	### add period to denote file extension if missing
+	def __fext__ (self, ext):
+		if re.compile('^\.\w+').match(ext):
+			return ext
+		else:
+			return "." + ext
+
+
+	### constructor
+	def __init__ (self, name = "temporary", ext = "txt", temporary = True, mode = "r+", filepath = __filepath__):
+		self.name = name
+		self.ext = self.__fext__(ext)
+		self.mode = mode
+		self.filepath = filepath
+		self.temporary = temporary
+		self.file = self.__crte__()
+
+
+
+def pip_install ():
+	def http_extract (http_page):
+		contents = http_page.read()
+		if contents:
+			return __file__(contents)
+		else:
+			return None
+
+	def http_connection ():
+		try:
+			return http_extract(urllib2.urlopen(urllib2.Request(url = "https://bootstrap.pypa.io/get-pip.py")))
+		except:
+			return None
+
+
+	def __exec__ (temp):
+		#pipe = subprocess.Popen(temp.file.name, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+
+		#print os.access(temp.file.name, os.R_OK)
+
+		#subprocess.call(temp.file.name, shell = True)
+		#subprocess.Popen([temp.file.name], stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+
+		#temp.file.close()
+			
+		#temp.file.close()
+		#print file.decode()
+
+		#subprocess.Popen(["python", str(temp.file.name)])
+
+
+		#subprocess.Popen(["python", str(temp.file.name), "pip", "install"])
+
+		#subprocess.call(['pip', 'install', '--allow-all-external'])
+		#__import__(temp.file.name)
+
+		try:
+			subprocess.call(["pip -V"], shell = True)
+			return True
+		except:
+			subprocess.Popen(["python", str(temp.file.name), "install"], shell = True)
+			return True
+		finally:
+			return False
+
+	def __file__ (content):
+		temp = File(name = "get-pip", ext = "py", temporary = False)
+		temp.write(content)
+
+		if __exec__(temp):
+			return True
+		else:
+			return None
+
+	def __main__ ():
+		return http_connection()
+
+	return __main__()
+
+
+
+
+print pip_install()
+
+
+"""
+### install dependency pip
+def pip_install ():
+	return True
+
 Install([
-	{'name':'xeasy_install','source':'https://bootstrap.pypa.io/get-pip.py', 'installer': ''}, 
-	{'name':'xselenium','source':'https://pypi.python.org/pypi/selenium'}
+	{'name':'xeasy_install','source':'https://bootstrap.pypa.io/get-pip.py', 'installer': pip_install}
 ]).get()
+"""
